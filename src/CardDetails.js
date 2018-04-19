36208835b9c9c10a39fa77460ed244b3fae6042c
ChingKing08 <austinchristopherking@gmail.com>
2018-04-19 16:59:51 -0400

    Added new card for testing and start of Cost for cards

--- src/CardDetails.js
+++ src/CardDetails.js
@@ -2,9 +2,9 @@ import React from 'react';
 import CardText from './CardText';
 import CardType from './CardType';
 
-export default ({ name, type, subtype, trigger, text, flavortext, power, toughness }) => 
+export default ({ name, type, subtype, trigger, text, flavortext, cost, power, toughness }) => 
   <div align="left" valign="top" >
-        <h2>{name}</h2>
+        <h2>{name}  {cost}</h2> 
         <hr />
         <CardType type={type} subtype={subtype} />
         <hr />
--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const styles = {
 
 const App = () => (
   <div style={styles}>
-    <Hello name="CodeSandbox" />
+    <Hello name="MTG Explorer" />
     <Card 
       name="Charging Monstrosaur" 
       image="https://img.scryfall.com/cards/large/en/xln/138.jpg"
@@ -18,6 +18,7 @@ const App = () => (
       subtype="Dinosaur"
       text="Trample, haste"
       flavortext="&quot;I knew I should have stayed with the boat. Always stay with the boat!&quot;"
+      cost="4R"
       power="5"
       toughness="5"
      />
@@ -29,8 +30,21 @@ const App = () => (
       trigger="Enrage"
       text=" - Whenever Ripjaw Raptor is dealt damage, draw a card."
       flavortext="Raptors are clever enough to tear away a hard metal shell to get at the tasty morsel inside."
+      cost="2GG"
       power="4"
       toughness="5"
+    />
+    <Card
+      name="Admiral Beckett Brass"
+      image="https://img.scryfall.com/cards/large/en/xln/217.jpg"
+      type="Legendary Creature"
+      subtype="Human Pirate"
+      trigger=""
+      text="Other Pirates you control get +1/+1.\nAt the beginning of your end step, gain control of target nonland permanent controlled by a player who was dealt combat damage by three or more Pirates this turn."
+      flavortext="&quot;You and your ship will make a fine addition to my fleet.&quot;"
+      cost="1UBR"
+      power="3"
+      toughness="3"
     />
     <h2>Start editing to see some magic happen {'\u2728'}</h2>
   </div>
