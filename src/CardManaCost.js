9aa0b699f61cad581ffee93de5afa5e57ac5cc78
ChingKing08 <austinchristopherking@gmail.com>
2018-04-20 17:11:53 -0400

    figured out string replace for CMC

--- src/CardManaCost.js
+++ src/CardManaCost.js
@@ -5,6 +5,7 @@ class CardManaCost extends React.Component {
 
     return (
       <div>
+        {this._convertManaCost()}
         <img width="25px" src="https://vignette.wikia.nocookie.net/mtg/images/d/da/Mana_W.png/revision/latest?cb=20130604114009" />
         <img width="25px" src="https://vignette.wikia.nocookie.net/mtg/images/a/a8/Mana_U.png/revision/latest?cb=20130604114015" />
         <img width="25px" src="https://vignette.wikia.nocookie.net/mtg/images/a/a6/Mana_B.png/revision/latest?cb=20130604114019" />
@@ -12,6 +13,13 @@ class CardManaCost extends React.Component {
         <img width="25px" src="https://vignette.wikia.nocookie.net/mtg/images/f/f7/Mana_G.png/revision/latest?cb=20130604114032" />
       </div>);
   }
+
+  _convertManaCost() {
+    var text = "1UBR";
+    text = text.replace('1', 'U');
+    return <div>{text}</div>
+  }
+
 }
 
 export default CardManaCost;
